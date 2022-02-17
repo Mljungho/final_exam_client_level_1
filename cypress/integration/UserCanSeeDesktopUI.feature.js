c4207bdc21bf14ed11d5fdcddc503ba91004adf3
Mljungho <mathias.ljungholm@gmail.com>
2022-02-17 11:18:03 +0100

    Added Viaplay API response fixture. Wrote basic e2e test to validate header content & main feed content

---
+++ cypress/integration/UserCanSeeDesktopUI.feature.js
@@ -0,0 +1,19 @@
+describe("User can visit website and view the main page.", () => {
+  before(() => {
+    cy.intercept("GET", "*/content.viaplay.se*", {
+      fixture: "viaplay_responce.json"
+    });
+    cy.visit("/");
+  });
+
+  describe("Header", () => {
+    it("is expected to see image in the header", () => {
+      cy.get("[cy-data=image-header]").should("exist");
+    });
+  });
+
+  describe("Main feed", () => {
+    it("is expected to see several program covers on the main page", () => {});
+    cy.get("[cy-data=program-feed]").should("exist");
+  });
+});
